1b73051e463a1eb8e21cbe864f6d58c033c12744
09Darshita09 <darshita09pangam@gmail.com>
2023-06-02 19:00:49 +0200

    edit in html file

--- app.py
+++ app.py
@@ -1,38 +1,41 @@
-from flask import Flask,redirect,url_for,render_template,request
- 
+from flask import Flask, request, render_template
+from src.pipeline.predict_pipeline import CustomData,PredictPipeline
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+
 app=Flask(__name__)
 
+## Route for a home page 
+
 @app.route('/')
-def welcome():
-    return render_template('index.html')
+def index():
+    return render_template('home.html') 
 
-@app.route('/success/<int:score>')
-def success(score):
-    res = ""
-    if score >= 50:
-        res = "PASS"
+@app.route('/predictdata',methods= ['POST','GET'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
     else:
-        res = "FAIL"
-    exp = {'score':score,'res':res}
-    return render_template('result.html',result=exp)
+        data = CustomData(
+            gender = request.form.get('gender'),
+            race_ethnicity = request.form.get('ethnicity'),
+            parental_level_of_education = request.form.get('parental_level_of_education'),
+            lunch = request.form.get('lunch'),
+            test_preparation_course = request.form.get('test_preparation_course'),
+            reading_score = request.form.get('reading_score'),
+            writing_score = request.form.get('writing_score'),
+        )
 
-### Result checker HTML Page
-@app.route('/submit',methods=['POST','GET'])
-def submit():
-    total_score = 0
-    if request.method == 'POST':
-        science = float(request.form['science'])
-        maths = float(request.form['maths'])
-        c = float(request.form['c'])
-        datascience = float(request.form['datascience'])
-        total_score = (science+maths+c+datascience)/4
-    res = ""
+        pred_df = data.get_data_as_dataframe()
+        print(pred_df)
 
-    if total_score >= 50:
-        res = "success"
-    else:
-        res = "fail"
-    return redirect(url_for('success',score=total_score))
+        predict_pipeline=PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+        return render_template('home.html',results=results[0])
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",debug=True)
